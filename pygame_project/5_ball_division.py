2c733f5bc84fa7cfc14bef1a7baa145d40abf8da
SUMMER <aibaisuru@gmail.com>
2020-08-01 23:52:03 +0900

    master

--- pygame_project/5_ball_division.py
+++ pygame_project/5_ball_division.py
@@ -192,6 +192,38 @@ while running:
             if weapon_rect.colliderect(ball_rect):
                 weapon_to_remove = weapon_idx   # 해당 무기 없애기 위한 값 설정
                 ball_to_remove = ball_idx   # 해당 공 없애기 위한 값 설정
+
+                # 가장 작은 크기의 공이 아니라면 다음 단계의 공으로 나눠주기
+                if ball_img_idx < 3:
+                    # 현재 공 크기 정보를 가지고 옴
+                    ball_width = ball_rect.size[0]
+                    ball_height = ball_rect.size[1]
+
+                    # 충돌 후 나눠진 공 정보
+                    # 현재보다 더 작은 공을 담아야하므로 공이 담긴 배열에서 다음 순서 인덱스를 불러와 담는다.
+                    small_ball_rect = ball_images[ball_img_idx + 1].get_rect()
+                    small_ball_width = small_ball_rect.size[0]
+                    small_ball_height = small_ball_rect.size[1]
+
+                    # 왼쪽으로 튕겨나가는 작은 공
+                    balls.append({
+                        "pos_x" : ball_pos_x + (ball_width / 2) - (small_ball_width / 2),
+                        "pos_y" : ball_pos_y + (ball_height / 2) - (small_ball_height / 2),
+                        "img_idx" : ball_img_idx + 1,   # 공의 이미지 인덱스
+                        "to_x" : -3, # 공의 x축 이동방향. -3 => 왼쪽으로 / 3 => 오른쪽으로 이동
+                        "to_y" : -6,    # 공의 y축 이동방향. 맨처음 약간 올라갔다가 내려가도록 설정
+                        "init_spd_y" : ball_speed_y[ball_img_idx + 1]  # y 최초 속도
+                    })
+
+                    # 오른쪽으로 튕겨나가는 작은 공
+                    balls.append({
+                        "pos_x" : ball_pos_x + (ball_width / 2) - (small_ball_width / 2),
+                        "pos_y" : ball_pos_y + (ball_height / 2) - (small_ball_height / 2),
+                        "img_idx" : ball_img_idx + 1,   # 공의 이미지 인덱스
+                        "to_x" : 3, # 공의 x축 이동방향. -3 => 왼쪽으로 / 3 => 오른쪽으로 이동
+                        "to_y" : -6,    # 공의 y축 이동방향. 맨처음 약간 올라갔다가 내려가도록 설정
+                        "init_spd_y" : ball_speed_y[ball_img_idx + 1]  # y 최초 속도
+                    })
                 break   # 충돌 시 탈출. 공, 무기 하나씩 없어지도록 설정할 것이므로
 
     # 충돌된 공 or 무기 없애기
